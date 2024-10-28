4d23a34a9e1d0026b5ed688fadb7b7fc06953247
jiepengtan <jiepengtan@gmail.com>
2024-10-28 16:23:32 +0800

    Temp commit for test

--- tools/ispx/run.bat
+++ tools/ispx/run.bat
@@ -1,11 +1,16 @@
 @echo off
+rd /s /q .builds
+if not exist .builds mkdir .builds
+copy ..\..\..\spx\tutorial\00-Hello\.builds\web\* .builds
+del .builds\gdspx.wasm
+
 if not exist .builds mkdir .builds
 setlocal
 
 set GOOS=js
 set GOARCH=wasm
 
-go build -tags canvas -o ./.builds/main.wasm main.go
+go build -tags canvas -o .builds/gdspx.wasm main.go
 
 endlocal
 
@@ -14,7 +19,6 @@ for /f "tokens=2" %%i in ('tasklist ^| findstr python') do taskkill /F /PID %%i
 :: Runs your python script
 
 cd .builds
-copy ..\index.html .
 copy ..\runner.html .
 copy ..\run.py .
 copy ..\test.zip .
@@ -27,5 +31,5 @@ if "%~1"=="" (
     set PORT=%~1
 )
 
-start  /B  python run.py %PORT%
+start  /B  python run.py -p %PORT%
 cd ..
